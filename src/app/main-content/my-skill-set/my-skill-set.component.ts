064a0dd929924c347c7327dab7937cf79de76ccd
SusanneRenken <renken.susanne@gmail.com>
2025-01-06 14:56:41 +0100

    My Skill section: add images

--- src/app/main-content/my-skill-set/my-skill-set.component.ts
+++ src/app/main-content/my-skill-set/my-skill-set.component.ts
@@ -5,8 +5,54 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './my-skill-set.component.html',
-  styleUrl: './my-skill-set.component.scss'
+  styleUrl: './my-skill-set.component.scss',
 })
 export class MySkillSetComponent {
 
+  skillIconsList = [
+    {
+      url: 'assets/img/angular.svg',
+      name: 'Angular',
+    },
+    {
+      url: 'assets/img/typescript.svg',
+      name: 'TypeScript',
+    },
+    {
+      url: 'assets/img/javascript.svg',
+      name: 'JavaScript',
+    },
+    {
+      url: 'assets/img/html.svg',
+      name: 'HTML',
+    },
+    {
+      url: 'assets/img/css.svg',
+      name: 'CSS',
+    },
+    {
+      url: 'assets/img/api.svg',
+      name: 'Rest-Api',
+    },
+    {
+      url: 'assets/img/firebase.svg',
+      name: 'Firebase',
+    },
+    {
+      url: 'assets/img/git.svg',
+      name: 'GIT',
+    },
+    {
+      url: 'assets/img/scrum.svg',
+      name: 'Scrum',
+    },
+    {
+      url: 'assets/img/materialdesign.svg',
+      name: 'Material design',
+    },
+    {
+      url: 'assets/img/challengeme.svg',
+      name: 'Challenge me',
+    },
+  ];
 }
