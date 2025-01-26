24633e274851e2d27539fb3e6d85b650daad2453
SusanneRenken <renken.susanne@gmail.com>
2025-01-26 16:16:22 +0100

    navbar in shared

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent],
+  imports: [CommonModule, RouterOutlet,NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { WhyMeComponent } from './why-me/why-me.component';
 import { MySkillSetComponent } from './my-skill-set/my-skill-set.component';
 import { MyWorkComponent } from './my-work/my-work.component';
@@ -11,7 +10,6 @@ import { ContactMeComponent } from './contact-me/contact-me.component';
   standalone: true,
   imports: [
     LandingPageComponent,
-    NavbarComponent,
     WhyMeComponent,
     MySkillSetComponent,
     MyWorkComponent,
