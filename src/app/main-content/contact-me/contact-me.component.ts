38e1c56db23247afc2961a3adfdd2a7eb53dcc60
SusanneRenken <renken.susanne@gmail.com>
2025-01-29 11:36:01 +0100

    contact-form outsourced to new component

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [ContactFormComponent],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
