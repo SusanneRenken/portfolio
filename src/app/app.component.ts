99edfa6f2a1da2613b1b9a8b022198452ca5f986
SusanneRenken <renken.susanne@gmail.com>
2025-02-04 09:24:10 +0100

    fix bug with routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MainContentComponent } from './main-content/main-content.component';
-import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,NavbarComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet ,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [ContactFormComponent],
+  imports: [ContactFormComponent, RouterModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
