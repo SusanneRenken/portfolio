0196c7c64e61c03b086a343f23727386de1d4f40
SusanneRenken <renken.susanne@gmail.com>
2025-02-14 16:18:20 +0100

    project card responsive

--- src/app/main-content/my-work/project-data.ts
+++ src/app/main-content/my-work/project-data.ts
@@ -14,9 +14,9 @@ export const PROJECTS: Projects[] = [
     id: 1,
     name: 'Join',
     aboutEn:
-      'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      'With Join, task management becomes a breeze – inspired by the Kanban system, I bring structure to your daily workflow. Thanks to its intuitive drag-and-drop feature, you can quickly create tasks, rearrange them, and effortlessly assign users and categories. This makes teamwork both more efficient and creatively engaging.',
     aboutDe:
-      'Aufgabenverwaltung inspiriert vom Kanban System. Erstelle und organisiere Aufgaben mit Drag-and-Drop-Funktionen, weise Benutzer und Kategorien zu.',
+      'Mit Join wird Aufgabenverwaltung zum Kinderspiel – inspiriert vom Kanban-System bringe ich Struktur in den Arbeitsalltag. Dank intuitiver Drag-and-Drop-Funktion kannst du Aufgaben schnell erstellen, neu anordnen und ganz einfach Benutzer sowie Kategorien zuweisen. So wird die Teamarbeit effizienter und gleichzeitig kreativer.',
     techStack: 'JavaScript | HTML | CSS | Firebase',
     image: 'join.jpg',
     repositoryUrl: 'https://github.com/SusanneRenken/join/',
@@ -27,9 +27,9 @@ export const PROJECTS: Projects[] = [
     id: 2,
     name: 'Sharkie',
     aboutEn:
-      'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.',
+      'With Sharkie, you dive into an exciting underwater adventure – steer a little shark, collect coins, and use poison. With the Bubble and Fin Attack, you strategically overcome dangerous foes and face the mighty final boss.',
     aboutDe:
-      'Ein einfaches Jump-and-Run-Spiel basierend auf einem objektorientierten Ansatz. Hilf Sharkie Münzen und Giftflaschen zu finden, um gegen den Schwertwal zu kämpfen.',
+      'Mit Sharkie tauchst du in ein spannendes Unterwasser-Abenteuer ein – steuere einen kleinen Hai, sammle Münzen und nutze Gift. Mit dem Bubble- und Fin Attack meisterst du gezielt gefährliche Gegner und stellst dich dem mächtigen Endboss.',
     techStack: 'JavaScript | HTML | CSS | Firebase',
     image: 'sharkie.jpg',
     repositoryUrl: 'https://github.com/SusanneRenken/sharkie/',
