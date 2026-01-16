78bec7e6c2dfe5f85b9717a9d1f8247835fc19ee
SusanneRenken <renken.susanne@gmail.com>
2026-01-17 00:17:18 +0100

    feat: add backend skills

--- src/app/main-content/my-skill-set/my-skill-set.component.ts
+++ src/app/main-content/my-skill-set/my-skill-set.component.ts
@@ -13,15 +13,24 @@ import { NavigationService } from '../../shared/services/navigation.service';
 export class MySkillSetComponent {
   skillIconsList = [
     { url: 'assets/img/angular.svg', name: 'Angular' },
+    { url: 'assets/img/rxjs.svg', name: 'RxJS' },
     { url: 'assets/img/typescript.svg', name: 'TypeScript' },
     { url: 'assets/img/javascript.svg', name: 'JavaScript' },
     { url: 'assets/img/html.svg', name: 'HTML' },
     { url: 'assets/img/css.svg', name: 'CSS' },
+    { url: 'assets/img/python.svg', name: 'Python' },
+    { url: 'assets/img/django.svg', name: 'Django' },
+    { url: 'assets/img/drf.svg', name: 'DRF' },
     { url: 'assets/img/api.svg', name: 'Rest-Api' },
-    { url: 'assets/img/firebase.svg', name: 'Firebase' },
+    { url: 'assets/img/postgresql.svg', name: 'PostgreSQL' },
+    { url: 'assets/img/redis.svg', name: 'Redis' },
+    { url: 'assets/img/docker.svg', name: 'Docker' },
     { url: 'assets/img/git.svg', name: 'GIT' },
-    { url: 'assets/img/scrum.svg', name: 'Scrum' },
-    { url: 'assets/img/materialdesign.svg', name: 'Material design' },
+    { url: 'assets/img/linux.svg', name: 'Linux' },
+    // { url: 'assets/img/firebase.svg', name: 'Firebase' },
+    // { url: 'assets/img/scrum.svg', name: 'Scrum' },
+    // { url: 'assets/img/materialdesign.svg', name: 'Material design' },
+    // { url: 'assets/img/sql.svg', name: 'SQL' },
   ];
 
   activeName: string | null = null;
