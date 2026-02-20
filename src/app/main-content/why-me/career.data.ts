cf4316561631c9b0603e6745baa3f7b33bed7958
SusanneRenken <renken.susanne@gmail.com>
2026-02-20 11:45:14 +0100

    edit career texts

--- src/app/main-content/why-me/career.data.ts
+++ src/app/main-content/why-me/career.data.ts
@@ -1,23 +1,35 @@
 export interface CareerEntry {
-    id: number;
-    titleKey: string;
-    descriptionKey: string;
+  id: number;
+  titleKey: string;
+  pointsKeys: string[];
 }
 
 export const CAREER_ENTRIES: CareerEntry[] = [
-    {
-        id: 1,
-        titleKey: 'CAREER_ENTRY_1_TITLE',
-        descriptionKey: 'CAREER_ENTRY_1_DESC',
-    },
-    {
-        id: 2,
-        titleKey: 'CAREER_ENTRY_2_TITLE',
-        descriptionKey: 'CAREER_ENTRY_2_DESC',
-    },
-    {
-        id: 3,
-        titleKey: 'CAREER_ENTRY_3_TITLE',
-        descriptionKey: 'CAREER_ENTRY_3_DESC',
-    },
+  {
+    id: 1,
+    titleKey: 'CAREER_ENTRY_1_TITLE',
+    pointsKeys: [
+      'CAREER_ENTRY_1_POINT_1',
+      'CAREER_ENTRY_1_POINT_2',
+      'CAREER_ENTRY_1_POINT_3',
+    ],
+  },
+  {
+    id: 2,
+    titleKey: 'CAREER_ENTRY_2_TITLE',
+    pointsKeys: [
+      'CAREER_ENTRY_2_POINT_1',
+      'CAREER_ENTRY_2_POINT_2',
+      'CAREER_ENTRY_2_POINT_3',
+    ],
+  },
+  {
+    id: 3,
+    titleKey: 'CAREER_ENTRY_3_TITLE',
+    pointsKeys: [
+      'CAREER_ENTRY_3_POINT_1',
+      'CAREER_ENTRY_3_POINT_2',
+      'CAREER_ENTRY_3_POINT_3',
+    ],
+  },
 ];
